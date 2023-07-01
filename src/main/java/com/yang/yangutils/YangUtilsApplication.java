51b9e6f70ab33453d8469f3b1611457a5d85ddb0
yang-sangwu <2728771838@qq.com>
2023-07-01 17:22:58 +0800

    11

---
+++ src/main/java/com/yang/yangutils/YangUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.yang.yangutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YangUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YangUtilsApplication.class, args);
+    }
+
+}
